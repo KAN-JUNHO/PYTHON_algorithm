5f488f5bf802f88872101128cfbd7553f638734f
junho <junho6667@gmail.com>
2021-04-03 23:09:14 +0900

    Revert "2021/04/03"
    This reverts commit ec36ef91

--- 동적계획법 1/RGB거리.py
+++ 동적계획법 1/RGB거리.py
@@ -6,5 +6,4 @@ for i in range(1, len(p)):
     p[i][0] = min(p[i-1][1],p[i-1][2])+p[i][0]
     p[i][1] = min(p[i-1][0],p[i-1][2])+p[i][1]
     p[i][2] = min(p[i-1][1],p[i-1][0])+p[i][2]
-    print(p)
 print(min(p[n-1]))
