6a16530b09690016c92795103b7b59e2aafb0f84
junho <junho6667@gmail.com>
2021-04-13 00:18:00 +0900

    2021/04/13

--- 큐,덱/AC.py
+++ 큐,덱/AC.py
@@ -1,44 +1,47 @@
 import sys
 from collections import deque
 
+if __name__ == "__main__":
+    T = int(sys.stdin.readline())
 
-T = int(sys.stdin.readline())
+    for _ in range(T):
+        operation = input()
+        n = int(sys.stdin.readline())
+        temp = input()
+        temp = temp[1:-1]  # [ ] 제거
 
-for _ in range(T):
-    operation = input()
-    n = int(sys.stdin.readline())
-    temp = input()
-    temp = temp[1:-1]
-
-    if "," not in temp and temp != "":
-        num_deque = deque([int(temp)])
-    elif temp != "":
-        num_deque = deque(map(int, temp.split(",")))
-    else:
-        num_deque = deque()
-
-    flag = 1
-    cnt = 0
-
-    for i in range(len(operation)):
-        if operation[i] == "R":
-            cnt += 1
+        if "," not in temp and temp != "":
+            num_deque = deque([int(temp)])
+        elif temp != "":
+            num_deque = deque(map(int, temp.split(",")))
         else:
-            if len(num_deque) == 0:
-                flag = 0
-                break
+            num_deque = deque()
 
-            if cnt % 2 == 0:
-                num_deque.popleft()
+        flag = 1
+        cnt = 0
+
+        for i in range(len(operation)):
+            if operation[i] == "R":
+                cnt += 1
             else:
-                num_deque.pop()
-    if cnt % 2 == 1:
-        num_deque.reverse()
+                if len(num_deque) == 0:
+                    flag = 0
+                    break
 
-    if flag:
-        num_deque = list(map(str,num_deque))
-        print("[", end="")
-        print(",".join(num_deque), end="")
-        print("]")
-    else:
-        print("error")
+                if cnt % 2 == 0:
+                    num_deque.popleft()
+                else:
+                    num_deque.pop()
+        if cnt % 2 == 1:
+            num_deque.reverse()
+
+        if flag:
+            print("[", end="")
+            for i in range(len(num_deque)):
+                if i == len(num_deque) - 1:
+                    print(num_deque[i], end="")
+                else:
+                    print(str(num_deque[i]) + ",", end="")
+            print("]")
+        else:
+            print("error")
