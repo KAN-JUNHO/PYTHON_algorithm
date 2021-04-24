2246831e6ce2c89aa116c768b0dbf90a70dcb2a4
junho <junho6667@gmail.com>
2021-04-24 22:34:31 +0900

    2021/04/24

--- 402 - 다이나믹 프로그래밍 1 (도전)/RGB거리 2.py
+++ 402 - 다이나믹 프로그래밍 1 (도전)/RGB거리 2.py
@@ -1,8 +1,32 @@
-n=int(input())
-paint = [list(map(int,input().split())) for i in range(n)]
-for i in range(1,n):
-    paint[i][0]=paint[i][0]+min(paint[i-1][1],paint[i-1][2])
-    paint[i][1]=paint[i][1]+min(paint[i-1][0],paint[i-1][2])
-    paint[i][2]=paint[i][2]+min(paint[i-1][0],paint[i-1][1])
+import sys
 
-print(paint)
+n = int(input())
+
+m = []
+
+for _ in range(n):
+    m.append([int(i) for i in sys.stdin.readline().split()])
+
+ret = 99123443214231
+
+for initcolor in range(3):
+
+    dp = [[0 for _ in range(n)] for _ in range(3)]
+
+    for i in range(3):
+        if i == initcolor:
+            dp[i][0] = m[0][i]
+            continue
+        dp[i][0] = 9123443214231
+
+    for i in range(1, n):
+        dp[0][i] = m[i][0] + min(dp[1][i - 1], dp[2][i - 1])
+        dp[1][i] = m[i][1] + min(dp[0][i - 1], dp[2][i - 1])
+        dp[2][i] = m[i][2] + min(dp[0][i - 1], dp[1][i - 1])
+
+    for i in range(3):
+        if i == initcolor:
+            continue
+        ret = min(ret, dp[i][-1])
+
+print(ret)
