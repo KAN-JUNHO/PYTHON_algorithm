2c6fece24e1a787cbc290a79799dc3969b708f8d
junho <junho6667@gmail.com>
2021-06-12 17:29:31 +0900

    2021/06/12

--- Greedy (그리디)/크게 만들기.py
+++ Greedy (그리디)/크게 만들기.py
@@ -1,11 +1,11 @@
 n,k=map(int,input().split())
 num=list(input())
 stack=[]
-
+cnt=k
 for i in range(n):
     while k>0 and stack and stack[-1]<num[i]:
         stack.pop()
         k-=1
     stack.append(num[i])
 
-print("".join(stack[:n-k]))
+print("".join(stack[:n-cnt]))
