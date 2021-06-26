d0556b174cae590657c979f05ead6d70ec251cd9
junho <junho6667@gmail.com>
2021-06-26 17:36:17 +0900

    2021/06/26

---
+++ Greedy (그리디)/비트 우정지수.py
@@ -0,0 +1,13 @@
+t=int(input())
+
+for _ in range(t):
+    n,m=map(str,input().split())
+    one=0
+    zero=0
+    for i in range(len(n)):
+        if n[i]!=m[i]:
+            if m[i]=="1":
+                one+=1
+            else:
+                zero+=1
+    print(max(one,zero))
